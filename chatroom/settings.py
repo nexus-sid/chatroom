f6a3931de31445dc782752943a72a1049b735acc
Sidharth Choudhury <128961342+nexus-sid@users.noreply.github.com>
2025-02-25 09:42:13 +0000

    Added ENV

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ez+0!i_p%zmmfv9kmqju9q_&3yl@+gxn$#m5+8w$pzwo3y^6ts"
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = str(os.getenv('DEBUG')) == '1'
 ALLOWED_HOSTS = ["*"]
 
 
