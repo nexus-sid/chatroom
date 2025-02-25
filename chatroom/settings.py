56cbfede934d44c6bb355e9a5ff92128ae20780d
Sidharth Choudhury <128961342+nexus-sid@users.noreply.github.com>
2025-02-25 10:19:17 +0000

    Chennel Version Changed

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -138,7 +138,8 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            # "hosts": [("127.0.0.1", 6379)],
+            "hosts": [os.getenv("REDIS_URL", "redis://localhost:6379")],
         },
     },
 }
